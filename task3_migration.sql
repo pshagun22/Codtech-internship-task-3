8283b0a17ad91b8a94973e14afe7c530c99e52ad
Shagun <pshagun229@gmail.com>
2025-02-15 13:16:33 +0530

    Add files via upload

---
+++ task3_migration.sql
@@ -0,0 +1,9 @@
+-- Task 3: Database Migration
+
+-- Export from MySQL
+-- Run this in the terminal
+-- mysqldump -u root -p --databases mydb > mydb.sql
+
+-- Import into PostgreSQL
+-- Run this in the terminal
+-- psql -U postgres -d mydb -f mydb.sql
